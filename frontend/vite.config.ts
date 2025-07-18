8bb640ddeeda373582edb76e4a3d4763797cdd81
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2025-07-18 17:54:13 -0500

    first commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+// frontend/vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // import from '@/components/...'
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+  },
+});
